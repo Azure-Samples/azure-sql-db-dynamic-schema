889024f1e5f82613a3f6cf5b4c72a786049404a9
Davide Mauri <damauri@microsoft.com>
2021-02-26 14:49:20 -0800

    updated database scripts

--- Database/cleanup.sql
+++ Database/cleanup.sql
@@ -1,27 +1,25 @@
 drop table if exists dbo.[todo_classic];
 drop table if exists dbo.[todo_hybrid];
-drop table if exists dbo.[todo_json];
-drop table if exists dbo.[todo_sample];
-drop table if exists dbo.[todo_test];
+drop table if exists dbo.[todo_document];
 
 drop sequence if exists dbo.[global_sequence];
 
 drop procedure if exists web.[get_todo_classic];
 drop procedure if exists web.[get_todo_hybrid];
-drop procedure if exists web.[get_todo_json];
+drop procedure if exists web.[get_todo_documentn];
 
 drop procedure if exists web.[post_todo_classic];
 drop procedure if exists web.[post_todo_hybrid];
-drop procedure if exists web.[post_todo_json];
+drop procedure if exists web.[post_todo_document];
 
 drop procedure if exists web.[patch_todo_classic];
 drop procedure if exists web.[patch_todo_hybrid];
-drop procedure if exists web.[patch_todo_json];
+drop procedure if exists web.[patch_todo_document];
 
 drop procedure if exists web.[delete_todo_classic];
 drop procedure if exists web.[delete_todo_hybrid];
-drop procedure if exists web.[delete_todo_json];
+drop procedure if exists web.[delete_todo_document];
 
-drop procedure if exists web.[get_todo_sample_classic];
-drop procedure if exists web.[get_todo_sample_json];
-drop procedure if exists web.[get_todo_sample_json2];
+drop procedure if exists web.[get_todo_test_classic]
+drop procedure if exists web.[get_todo_test_hybrid
+drop procedure if exists web.[get_todo_test_document]
