a97408a7261c67f0638d0b35cb1dc140835047bd
Davide Mauri <damauri@microsoft.com>
2024-03-04 14:58:52 -0800

    handle both SQL Server and Azure SQL

--- Database/00-create.sql
+++ Database/00-create.sql
@@ -1,9 +1,30 @@
+-- Create schema
 create schema [web] authorization [dbo];
 go
 
-create user [dynamic-schema-test-user] with password = 'Super_Str0ng*P@ZZword!'
-go
+-- Create application user 
+if (serverproperty('Edition') = 'SQL Azure') begin
 
+    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'dynamic-schema-test-user')
+    begin 
+        create user [dynamic-schema-test-user] with password = 'Super_Str0ng*P@ZZword!'
+    end
+
+end else begin
+
+    if not exists (select * from sys.server_principals where [type] in ('E', 'S') and [name] = 'dynamic-schema-test-user')
+    begin 
+        create login [dynamic-schema-test-user] with password = 'Super_Str0ng*P@ZZword!'
+    end    
+
+    if not exists (select * from sys.database_principals where [type] in ('E', 'S') and [name] = 'dynamic-schema-test-user')
+    begin
+        create user [dynamic-schema-test-user] from login [dynamic-schema-test-user]            
+    end
+
+end
+
+-- Grant permission to the application user
 grant execute on schema::[web] to [dynamic-schema-test-user]
 go
 
